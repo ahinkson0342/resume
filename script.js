96720fba4b6c7f5af58cc09589e325d4c00fd836
adh1056 <adh1056@comcast.net>
2024-01-09 19:39:57 -0800

    Added navbar functionality, along with dark mode toggle

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.querySelector("#btnSwitch").addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    }
+})
